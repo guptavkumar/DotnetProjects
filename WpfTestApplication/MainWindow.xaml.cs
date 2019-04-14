e6b4fd15b73eeb38d10eb6d177fa83c72c2d0020
guptavkumar <guptavkumar@gmail.com>
2019-04-14 23:11:29 +0530

    Upload first Dot net project

---
+++ WpfTestApplication/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace WpfTestApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Added Comment
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            listbox1.Items.Clear(); 
+            var stopWatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < 9000000; i++)
+            {
+                listbox1.Items.Add("List Item "+i);
+            }
+            stopWatch.Stop();
+
+            lblThreadResult.Content = "Finished in " + (double)stopWatch.ElapsedMilliseconds / 1000 + " Seconds";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello");
+            listbox1.Items.Clear();
+            var stopWatch = Stopwatch.StartNew();
+
+            ThreadStart startThread = new ThreadStart(() =>
+                {
+                    for (long i = 0; i < 900000000; i++)
+                    {
+                        listbox1.Items.Add("List Item " + i);
+                    }
+                    stopWatch.Stop();
+
+                    lblThreadResult.Content = "Finished in " + (double)stopWatch.ElapsedMilliseconds / 1000 + " Seconds";
+                });
+            Thread t = new Thread(startThread);
+            t.Start();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            listbox1.Items.Clear();
+            Stopwatch stpWatch = Stopwatch.StartNew();
+            //ThreadStart job = new ThreadStart(delegate()
+            //{
+            //    {
+                    for (int i = 0; i < 200000; i++)
+                    {
+                        listbox1.Dispatcher.Invoke(DispatcherPriority.SystemIdle, new Action(delegate()
+                        {
+                            listbox1.Items.Add("List item " + i);
+                        }));
+                    }
+            //    };
+            //});
+            //stpWatch.Stop();
+                    listbox1.Dispatcher.Invoke(DispatcherPriority.SystemIdle, new Action(delegate()
+               {
+               lblThreadResult.Content="Finishid in "+stpWatch.ElapsedMilliseconds/1000+" Seconds";}));
+           //Thread th = new Thread(job);
+        //   th.Start();
+        }
+
+      
+    }
+}
---
+++ WpfTestApplication/MyUserControl.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace WpfTestApplication
+{
+    /// <summary>
+    /// Interaction logic for MyUserControl.xaml
+    /// </summary>
+    public partial class MyUserControl : UserControl,INotifyPropertyChanged
+    {
+        public MyUserControl()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChange(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+
+
+        public string ControlText
+        {
+            get { return (string)GetValue(ControlTextProperty); }
+            set { SetValue(ControlTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Caption.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ControlTextProperty =
+            DependencyProperty.Register("ControlText", typeof(string), typeof(MyUserControl), new PropertyMetadata(string.Empty, OnCaptionPropertyChanged));
+
+        private static void OnCaptionPropertyChanged(DependencyObject dependencyObject,DependencyPropertyChangedEventArgs e)
+        {
+            MyUserControl myControl = dependencyObject as MyUserControl;
+            myControl.RaisePropertyChange("ControlText");
+            myControl.OnCaptionPropertyChanged();
+        }
+        
+        private void OnCaptionPropertyChanged()
+        {
+        //    txbCaption.Text=ControlText;
+        }
+
+
+
+
+        public string ForeColour
+        {
+            get { return (string)GetValue(ForeColourProperty); }
+            set { SetValue(ForeColourProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ForeColour.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ForeColourProperty =
+            DependencyProperty.Register("ForeColour", typeof(string), typeof(MyUserControl), new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.None, PropertyChangeCallback, OnCoerceCurrentTimeProperty), OnValidateCurrentTimeProperty);
+
+        private static void PropertyChangeCallback(DependencyObject obj,DependencyPropertyChangedEventArgs e)
+        {
+            MyUserControl myControl = obj as MyUserControl;
+            myControl.RaisePropertyChange("ForeColour");
+            myControl.OnColorPropertyChange();
+        }
+
+        private void OnColorPropertyChange()
+        {
+            txbCaption.Foreground = new SolidColorBrush((Color)TypeDescriptor.GetConverter(typeof(Color)).ConvertFromString(this.ForeColour));
+        }
+
+        private static object OnCoerceCurrentTimeProperty(DependencyObject sender, object data)
+        {
+            if (data.ToString().Length > 3)
+                data = data.ToString().Substring(0, 3);
+            return data;
+        }
+
+        private static bool OnValidateCurrentTimeProperty(object data)
+        {
+            if (data.ToString() == "Red")
+                return true;
+            else return false;
+        }
+
+
+        #region ReadOnly DP
+
+
+        public bool IsMouseOver
+        {
+            get { return (bool)GetValue(IsMouseOverProperty); }
+            set { SetValue(IsMouseOverProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsMouseOver.  This enables animation, styling, binding, etc...
+        public static readonly DependencyPropertyKey IsMouseOverPropertyKey =
+            DependencyProperty.RegisterReadOnly("IsMouseOver", typeof(bool), typeof(MyUserControl),new PropertyMetadata(false));
+
+        public static readonly DependencyProperty IsMouseOverProperty = IsMouseOverPropertyKey.DependencyProperty;
+        #endregion
+    }
+}
